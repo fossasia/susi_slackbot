11af009857cc282ce16285f7b01e3b1abddc29e7
Ali Ayub Khan <akhan.bscs15seecs@seecs.edu.pk>
2017-07-19 01:24:17 +0500

    Made slack bot using real time API and removed bot functions (#30)
    * configured slack bot
    
    * Uses real time API to make skype bot and removed other bot functions

--- index.js
+++ index.js
@@ -1,244 +1,73 @@
-'use strict';
-/* global require, process, console */
-
-var express = require('express');
-var bodyParser = require('body-parser');
+var Slack = require('@slack/client');
 var request = require('request');
-var http = require("http");
-var TelegramBot = require('node-telegram-bot-api');
-var SlackBot = require('slackbots');
-var Slack = require('node-slackr')
-
+var express = require('express');
+var http = require('http');
 var app = express();
-app.set('port', (process.env.PORT || 5000));
 
-app.use(bodyParser.urlencoded({extended: false}));
+var RtmClient = Slack.RtmClient;
+var RTM_EVENTS = Slack.RTM_EVENTS;
 
-app.use(bodyParser.json());
+var token = process.env.token;
 
-// recommended to inject access tokens as environmental variables, e.g.
-var token = process.env.FB_PAGE_ACCESS_TOKEN;
-var telegramToken = process.env.TELEGRAM_ACCESS_TOKEN;
-var slack_webhook_url = process.env.SLACK_WEBHOOK_URL; //url of incoming webhook
-var heroku_deploy_url = (process.env.HEROKU_URL)||("https://damp-everglades-14739.herokuapp.com/"); //url of heroku deployment
-// Setup polling way
-var bot = new TelegramBot(telegramToken, {polling: true});
-// const token = "<PAGE_ACCESS_TOKEN>"
-var slack_token = process.env.SLACK_TOKEN;
-var slack_bot = new SlackBot({
-	token: slack_token, 
-	name: 'susi'
-})
-var payload;
-var slack = new Slack(slack_webhook_url);
+var rtm = new RtmClient(token, { logLevel: 'info' });
+rtm.start();
 
-// FACEBOOK SERVICE FOR SUSI
-// ----------------------------------------------------------------------------------------------------------------
+rtm.on(RTM_EVENTS.MESSAGE, function(message) {
+  var channel = message.channel;
 
-function sendTextMessage(sender, text) {
-	var messageData = { text:text };
-	
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token:token},
-		method: 'POST',
-		json: {
-			recipient: {id:sender},
-			message: messageData,
-		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log('Error sending messages: ', error);
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error);
-		}
-	});
-}
+  var options = {
+        method: 'GET',
+        url: 'http://api.asksusi.com/susi/chat.json',
+        qs: {
+            timezoneOffset: '-330',
+            q: message.text
+        }
+    };
+//sending request to SUSI API for response
+    request(options, function(error, response, body) {
+        if (error){
+            msg = "Oops, looks like SUSI is taking a break, She will be back soon";
+            rtm.sendMessage(msg, channel);
+        } else {
+        
+            var type = (JSON.parse(body)).answers[0].actions;
+		    var msg;
+            if (type.length == 1 && type[0].type == "answer") {
+                msg = (JSON.parse(body)).answers[0].actions[0].expression;
+                rtm.sendMessage(msg, channel);
+            } else if (type.length == 1 && type[0].type == "table") {
+                var data = (JSON.parse(body)).answers[0].data;
+                var columns = type[0].columns;
+                var key = Object.keys(columns);
+                var count = (JSON.parse(body)).answers[0].metadata.count;
 
-function sendGenericMessage(sender, title, subtitle, image_url) {
-	var messageData = {
-		"attachment": {
-			"type": "template",
-			"payload": {
-				"template_type": "generic",
-				"elements": [{
-					"title": title,
-					"subtitle": subtitle,
-					"image_url": image_url
-				}]
-			}
-		}
-	};
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token:token},
-		method: 'POST',
-		json: {
-			recipient: {id:sender},
-			message: messageData,
-		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
-		}
-	})
-}
+                for (var i = 0; i < count ; i++) {
+                    msg = key[0].toUpperCase() + ": " + data[i][key[0]] + "\n" + key[1].toUpperCase() + ": " + data[i][key[1]] + "\n" + key[2].toUpperCase() + ": " + data[i][key[2]];
+                    rtm.sendMessage(msg, channel);
+                }
+            } else if (type.length == 2 && type[1].type == "rss"){
+                var data = JSON.parse(body).answers[0].data;
+                var columns = type[1];
+                var key = Object.keys(columns);
 
-app.get('/', function (req, res) {
-	res.send('Susi says Hello.');
+                for (var i = 0; i < 4; i++) {
+                if(i==0){
+                    msg = (JSON.parse(body)).answers[0].actions[0].expression;
+                    rtm.sendMessage(msg, channel);
+                    console.log("check");
+                } else{
+                    msg = "";
+                    msg =key[1].toUpperCase() + ": " + data[i][key[1]] + "\n" + key[2].toUpperCase() + ": " + data[i][key[2]] + "\n" + key[3].toUpperCase() + ": " + data[i][key[3]];
+                    rtm.sendMessage(msg, channel);
+                                    console.log("check");
+                }
+                }
+            }
+        }
+    })
 });
 
-// for facebook verification
-app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-		res.send(req.query['hub.challenge']);
-	}
-	res.send('Error, wrong token');
-});
-
-// to post data
-app.post('/webhook/', function (req, res) {
-	var messaging_events = req.body.entry[0].messaging
-	for (var i = 0; i < messaging_events.length; i++) {
-		var event = req.body.entry[0].messaging[i];
-		var sender = event.sender.id;
-		if (event.message && event.message.text) {
-			var text = event.message.text;
-			if (text === 'image') {
-				// Sample testing URL
-				sendGenericMessage(sender, 'Map Location', 'This is the location', 'http://loklak.org/vis/map.png?mlat=17.77262&mlon=78.2728192&zoom=12');
-				// Images are sent similar to this.
-				// Implement actual logic later here.
-				continue
-			}
-
-			// Construct the query for susi
-			var queryUrl = 'http://loklak.org/api/susi.json?q='+encodeURI(text);
-			var message = '';
-			// Wait until done and reply
-			request({
-				url: queryUrl,
-				json: true
-			}, function (error, response, body) {
-				if (!error && response.statusCode === 200) {
-					message = body.answers[0].actions[0].expression;
-					sendTextMessage(sender, message);
-				} else {
-					message = 'Oops, Looks like Susi is taking a break, She will be back soon';
-					sendTextMessage(sender, message);
-				}
-			});
-			// sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-		}
-		if (event.postback) {
-			var text = JSON.stringify(event.postback);
-			sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token);
-			continue;
-		}
-	}
-	res.sendStatus(200)
-})
-
-
-// TELEGRAM SERVICE FOR SUSI
-// ----------------------------------------------------------------------------------------------------------------
-// Matches /start 
-bot.onText(/\/start/, function (msg, match) {
-	var fromId = msg.from.id;
-	var resp = 'Hi, I am Susi, You can ask me anything !';
-	bot.sendMessage(fromId, resp);
-});
-
-// Any other message
-bot.on('message', function (msg) {
-	var chatId = msg.chat.id;
-	var queryUrl = 'http://loklak.org/api/susi.json?q='+encodeURI(msg.text);
-	var message = '';
-	// Wait until done and reply
-	if (msg.text !== '/start') {
-		request({
-			url: queryUrl,
-			json: true
-		}, function (error, response, body) {
-			if (!error && response.statusCode === 200) {
-				message = body.answers[0].actions[0].expression;
-				bot.sendMessage(chatId, message);
-			} else {
-				message = 'Oops, Looks like Susi is taking a break, She will be back soon';
-				bot.sendMessage(chatId, message);
-			}
-		});
-	}
-});
-
-//SLACK SERVICE FOR SUSI
-//-----------------------------------------------------------------------------------------------------------------
-function slackbot(){
-
-	setInterval(function() {
-		http.get(heroku_deploy_url);
-	}, 1800000); //pings the deployment url every 30 minutes
-
-	slack_bot.on('message', function(data){
-		var slackdata = data;
-		var msg, channel, output, user;
-		if(Object.keys(slackdata).length > 0){
-			if('text' in slackdata && slackdata['username'] != 'susi'){
-				msg = data['text'];
-				channel = data['channel']
-			}
-			else {
-				msg = null;
-				channel = null;
-			}
-		}
-		if(msg != null && channel !=null){
-			var botid = '<@U1UK6DANT>:' 
-			if (msg.split(" ")[0] != botid){
-			//do nothing
-		} else{
-			var apiurl = 'http://loklak.org/api/susi.json?q=' + msg;
-			var payload;
-			request(apiurl, function (error, response, body) {
-				if (!error && response.statusCode === 200) {
-					var data = JSON.parse(body);
-					if(data.answers[0].actions.length == 1){
-						var susiresponse = data.answers[0].actions[0].expression;
-						payload = {
-							text: susiresponse,
-							channel: channel
-						}
-						slack.notify(payload)
-
-					} else if(data.answers[0].actions.length == 2 && data.answers[0].actions[1].type == "table"){
-						payload = {
-							text: data.answers[0].actions[0].expression + " (" + data.answers[0].data.length + " results)",
-							channel: channel
-						}
-						slack.notify(payload)
-						for(var i = 0; i < data.answers[0].data.length; ++i){
-							var response = data.answers[0].data[i];
-							var ansstring = "";
-							for(var resp in response){
-								ansstring += (resp + ": " + response[resp] + ", ");
-							}
-							payload = {
-								text: ansstring,
-								channel: channel
-							}
-							slack.notify(payload);
-						}
-					}
-				}
-			});
-		}
-	}
-});
-}
-// Getting Susi up and running.
-app.listen(app.get('port'), function() {
-	console.log('running on port', app.get('port'));
-	slackbot();
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
 });
